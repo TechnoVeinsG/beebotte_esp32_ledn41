ecd964954647b62d4d2c4b29359cacef9b0925d9
TechnoVeinsG <38637891+TechnoVeinsG@users.noreply.github.com>
2018-08-02 15:51:25 +0900

    Add files via upload

---
+++ beebotte_esp32_ledn41.ino
@@ -0,0 +1,125 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "config.h"
+
+#define MQTT_MAX_PACKET_SIZE 1024
+
+WiFiClient wifiClient;
+PubSubClient MQTTClient(host, mqttPort, wifiClient);
+IPAddress ledn41_IP(ip[0],ip[1],ip[2],ip[3]);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  Serial.println("MQTT message arrived");
+  
+  char buffer[MQTT_MAX_PACKET_SIZE];
+  StaticJsonBuffer<MQTT_MAX_PACKET_SIZE> jsonBuffer;
+
+  // Beebotteから受信したペイロードをbufferにコピー
+  snprintf(buffer, sizeof(buffer), "%s", payload);
+  
+  // json形式のデータbufferをパース
+  JsonObject& root = jsonBuffer.parseObject(buffer);
+
+  if (!root.success()) {
+    Serial.println("parseObject failed");
+    return;
+  }
+
+  // dataの値を取得
+  const char* parsedData = root["data"];
+
+  if (parsedData != NULL) {
+    
+    // dataの値がdisplay_onの場合、LEDN41に表示コマンドを送信
+    if (strcmp(parsedData, "display_on") == 0) {
+      
+      Serial.println("got display_on from beebotte");
+
+      // LEDN41に接続
+      if (wifiClient.connect(ledn41_IP, port)) {
+        
+        Serial.println("connected to ledn41");
+        
+        // LEDN41にコマンド送信
+        wifiClient.write(ledn41_cmd, sizeof(ledn41_cmd));
+
+      }
+      
+    }
+    
+  }
+
+}
+
+void setup() {
+  
+  Serial.begin(115200);
+  Serial.println();
+  
+  WiFi.mode(WIFI_STA);
+  
+}
+
+void loop() {
+  
+  // Wi-Fi接続されていない場合
+  if (WiFi.status() != WL_CONNECTED) {
+    
+    Serial.print("connecting to ");
+    Serial.print(ssid);
+    Serial.println("...");
+    
+    WiFi.begin(ssid, password);
+    
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      
+      Serial.println("Failed to connect");
+      delay(5000);
+      return;
+      
+    } else {
+      
+      Serial.print("Wi-Fi connected: ");
+      Serial.println(WiFi.localIP());
+      
+    }
+    
+  }
+
+  // Beebotteに接続されていない場合
+  if (!MQTTClient.connected()) {
+    
+    String tokenname = "token:";
+    tokenname += channelToken;
+    
+    // Beebotteに接続
+    MQTTClient.connect(clientID, tokenname.c_str(), NULL);
+    
+    if (MQTTClient.connected()) {
+      
+      Serial.println("MQTT connected");
+      
+      // トピック名を指定してsubscriberに設定
+      MQTTClient.subscribe(topic);
+      
+      // Beebotteからのメッセージ受信時に呼ばれる関数を設定
+      MQTTClient.setCallback(callback);
+      
+    } else {
+      
+      Serial.print("MQTT connection failed: ");
+      Serial.println(MQTTClient.state());
+      delay(5000);
+      
+    }
+    
+  } else {
+    
+    // Beebotteとの接続をキープ
+    MQTTClient.loop();
+    
+  }
+
+}
---
+++ config.h
@@ -0,0 +1,20 @@
+// LEDN41に1234と表示させるコマンド
+const char ledn41_cmd[8] = {0x1B, 0x30, 0x44, 0x31, 0x32, 0x33, 0x34, 0x0D};
+// LEDN41のIPアドレス
+const uint8_t ip[4] = {192,168,1,199};
+// LEDN41のポート番号
+const uint16_t port = 10001;
+// MQTTホスト名
+const char* host = "mqtt.beebotte.com";
+// MQTTポート番号
+const uint16_t mqttPort = 1883;
+// クライアントID
+const char *clientID = "TVC_ESP32";
+// トピック名
+const char* topic = "control_ledn41/display";
+// Channel Token
+const char* channelToken = "token_xxxxxxxxxxxxxxxx";
+// Wi-Fi SSID
+const char *ssid = "YOUR SSID";
+// Wi-Fi Password
+const char *password = "YOUR PASSWORD";
